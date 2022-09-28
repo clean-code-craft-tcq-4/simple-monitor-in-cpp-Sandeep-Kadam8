ad745f2c504a020b68007d6e69df04e29d154405
Sandeep Kadam <105204457+Sandeep-Kadam8@users.noreply.github.com>
2022-09-28 12:59:59 +0530

    Create Parameter.h

---
+++ Parameter.h
@@ -0,0 +1,41 @@
+#ifndef PARAMETER_H_
+#define PARAMETER_H_
+
+#include <string>
+
+namespace Battery {
+
+template <typename T>
+class Parameter {
+ public:
+    Parameter(const T& value, const T& minValue, const T& maxValue, const std::string& parameterName) {
+        this->value = value;
+        this->minValue = minValue;
+        this->maxValue = maxValue;
+        this->parameterName = parameterName;
+    }
+    virtual ~Parameter();
+
+    T GetValue() const {
+        return value;
+    }
+    T GetUpperLimit() const {
+        return maxValue;
+    }
+    T GetLowerLimit() const {
+        return minValue;
+    }
+
+    std::string GetParameterName() const {
+        return parameterName;
+    }
+
+ private:
+    T value;
+    T minValue;
+    T maxValue;
+    std::string parameterName;
+};
+
+} // namespace Battery
+#endif /* PARAMETER_H_ */
