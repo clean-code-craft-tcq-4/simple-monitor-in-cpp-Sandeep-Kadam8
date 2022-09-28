5a382cd39070cb840e133610bba189a951ebc764
Sandeep Kadam <105204457+Sandeep-Kadam8@users.noreply.github.com>
2022-09-28 18:49:20 +0530

    Create HealthCheckerTest.cpp

---
+++ HealthCheckerTest.cpp
@@ -0,0 +1,8 @@
+#include "HealthChecker.h"
+#include <assert.h>
+
+int main() {
+  Battery::HealthChecker BatteryHealthchecker;
+  assert(BatteryHealthchecker.IsOk(25, 70, 0.7) == true);  
+  assert(BatteryHealthchecker.IsOk(50, 85, 0) == false);
+}
