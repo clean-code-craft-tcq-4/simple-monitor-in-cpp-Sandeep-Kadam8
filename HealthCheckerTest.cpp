278a2d4865f9a36ca28041dded0209d7174ebaa8
Sandeep Kadam <105204457+Sandeep-Kadam8@users.noreply.github.com>
2022-10-04 15:32:29 +0530

    Update HealthCheckerTest.cpp

--- HealthCheckerTest.cpp
+++ HealthCheckerTest.cpp
@@ -6,8 +6,8 @@
 
 void TestIsOk() {
   Battery::HealthChecker BatteryHealthchecker;
-  assert(BatteryHealthchecker.IsOk(25, 70, 0.7) == true);
-  assert(BatteryHealthchecker.IsOk(50, 85, 0) == false);
+  assert(BatteryHealthchecker.IsOk(50, 70, 0.7) == true);
+  assert(BatteryHealthchecker.IsOk(75, 85, 0) == false);
 }
 
 void TestIsParameterOutOfRange() {
@@ -15,11 +15,19 @@ void TestIsParameterOutOfRange() {
   Battery::Temperature t(50, 100, 200);
   assert(BatteryHealthchecker.IsParameterOutOfRange(t) == true);
   Battery::StateOfCharge soc(5.0, 4.0, 5.0);
-  assert(BatteryHealthchecker.IsParameterOutOfRange(soc) == false); 
+  assert(BatteryHealthchecker.IsParameterOutOfRange(soc) == false);
   Battery::ChargeRate chargeRate(0.8, 0.0, 0.8);
-  assert(BatteryHealthchecker.IsParameterOutOfRange(chargeRate) == false);  
+  assert(BatteryHealthchecker.IsParameterOutOfRange(chargeRate) == false);
+}
+
+void TestCheckForEarlyWarning() {
+    Battery::HealthChecker BatteryHealthchecker;
+    Battery::StateOfCharge soc(78.0);
+    BatteryHealthchecker.CheckForEarlyWarning(soc, 5);
 }
 
 int main() {
   TestIsOk();
+  TestIsParameterOutOfRange();
+  TestCheckForEarlyWarning();
 }
