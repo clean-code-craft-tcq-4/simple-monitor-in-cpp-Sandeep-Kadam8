319bea9e131fc9e90e998b06ce89bcc7d3acff13
Sandeep Kadam <105204457+Sandeep-Kadam8@users.noreply.github.com>
2022-10-04 13:54:13 +0530

    Update HealthChecker.h

--- HealthChecker.h
+++ HealthChecker.h
@@ -7,6 +7,13 @@
 #include <functional>
 
 namespace Battery {
+ 
+#define SOC_WARNING_TOLERANCE_PERCENT 5
+
+enum MessageState {
+    Warning,
+    Alarm
+};
 
 class HealthChecker final {
  public:
@@ -19,9 +26,12 @@ class HealthChecker final {
     bool IsParameterOutOfRange(const Parameter<T>& parameter) {
         return (parameter.GetValue() < parameter.GetLowerLimit() || parameter.GetValue() > parameter.GetUpperLimit());
     }
-
-    void WarningOutOfRange(const std::string & parameterName, std::function<void(const std::string&)> notify);
-    std::string PrepareParameterMessage(const std::string& parameterName, const std::string& status);
+ 
+    float GetScaledParameterValue(const float &value, const float &min, const float &max);
+    void CheckForEarlyWarning(const Parameter<float> &parameter, int warningTolerancePercent);
+    void WarningAlert(const std::string & parameterName, const std::string &status, std::function<void(const std::string&)> function);
+    void AlarmOutOfRange(const std::string & parameterName, std::function<void(const std::string&)> function);
+    std::string PrepareParameterMessage(const std::string& parameterName, MessageState state, const std::string& status);
     static void printOnConsole(const std::string& data);
  private:
     const std::string OUTOFRANGE = "out of range!";
